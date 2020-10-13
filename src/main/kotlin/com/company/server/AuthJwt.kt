cb3316075456f737d37d0d380cf69f4375b8cba6
Felipe Araujo <felipe.ar@pm.me>
2020-10-13 19:55:12 -0300

    AuthJwt

--- src/main/kotlin/com/company/server/AuthJwt.kt
+++ src/main/kotlin/com/company/server/AuthJwt.kt
@@ -3,8 +3,8 @@ package com.company.server
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 
-open class AuthJwt(val key: String ) {
-    private val algorithm = Algorithm.HMAC256(key)
+open class AuthJwt(val secret: String ) {
+    private val algorithm = Algorithm.HMAC256(secret)
     val verifier = JWT.require(algorithm).build()
 
 
